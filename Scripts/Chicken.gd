cba8133cc51232c17606d68f5cd371ed5fe2ce61
solaraloe <aldenw100@gmail.com>
2023-07-07 23:05:14 -0700

    aw piss

--- Scripts/Chicken.gd
+++ Scripts/Chicken.gd
@@ -2,4 +2,6 @@ extends Animal
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# test code REMEMBER TO COMMENT THIS OUT
+	super()
 	Spawn(50, 2, 1, 1, 100, Vector2(500, 500))
