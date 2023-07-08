cdc339dc688a2662ebe77106732c6fba5be8c59c
solaraloe <aldenw100@gmail.com>
2023-07-07 20:33:37 -0700

    erm

---
+++ Scripts/Animal.gd
@@ -0,0 +1,14 @@
+class_name Animal extends CharacterBody2D
+
+var moveDistance = 100
+var moveInterval = 2
+var stepsPerMovement = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
