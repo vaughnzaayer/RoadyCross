4294033d44b8b5565079441563b0411201b88d47
solaraloe <aldenw100@gmail.com>
2023-07-07 22:37:47 -0700

    animal movement prototype

--- Scripts/Animal.gd
+++ Scripts/Animal.gd
@@ -1,20 +1,39 @@
 class_name Animal extends Node2D
 
-var moveDistance = 100
+# moveDistance: movement per movement opportunity
+# moveInterval: time between movement opportunities
+# stepsPerMovement: amount of steps to split movement distance into
+# lerpWeight: snappiness of animal movement
+# lerpWeight is a float from 0-1, 0 = no movement, 1 = near instant movement
+
+var moveDistance = 50
 var moveInterval = 2
 var stepsPerMovement = 1
-var lerpWeight = 0.24
+var lerpWeight = 0.14
+
+# health: the damage an animal can take before dying
+# points: the amount of points this animal grants to the player upon dying
 
 var health = 1
 var points = 100
 
-var movesLeft = 0
+# movesLeft: an internal variable used for step movement, counts how many steps are left in the current movement opportunity
+# safeDist: the distance (squared) that the animal must be within their target position for their step to count as a successful step
 
-var safeDist = 0.05
+var movesLeft = 0
+var safeDist = 1
+
+# moveTimer: the animal's internal timer that tells it when to move
+# targetPosition: the animal's target position. updated at the start of every step, and the animal will then start to move towards it.
+# collision: the animal's Area2D that tells it when it's collided with something
 
 @onready var moveTimer = $Timer
-@onready var targetPosition = $Marker2D
+var targetPosition = Vector2.ZERO
 @onready var collision = $Area2D
+
+# state: the animal's current state, used in the animal's FSM
+# prevState: the previous state the animal was in. idk when this will be useful but I made it anyways just in case
+# states: an enum containing all of the possible states an animal can be in
 
 var state = states.IDLE
 var prevState = null
@@ -24,12 +43,16 @@ enum states {
 	IDLE
 }
 
-func bruh(dist, interval, steps, health, points):
+# call Spawn after instancing an animal to "activate" them.
+# kinda like _init but godot automatically calls _init when an object is instantiated and that's cringe
+func Spawn(dist, interval, steps, health, points, pos):
 	moveDistance = dist
 	moveInterval = interval
 	stepsPerMovement = steps
 	self.health = health
 	self.points = points
+	
+	global_position = pos
 	
 	moveTimer.timeout.connect(Callable(self, "MoveTimeout"))
 	
@@ -38,30 +61,35 @@ func bruh(dist, interval, steps, health, points):
 	
 	ChangeState(states.IDLE)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# this is where the bulk of the animal FSM is contained
 func _process(delta):
 	match state:
 		states.MOVING:
-			global_position.y = lerp(global_position.y, targetPosition.global_position.y, lerpWeight)
-			global_position.x = lerp(global_position.x, targetPosition.global_position.x, lerpWeight)
+			# move towards target position
+			global_position.x = lerp(global_position.x, targetPosition.x, lerpWeight)
+			global_position.y = lerp(global_position.y, targetPosition.y, lerpWeight)
 			
-			if global_position.distance_squared_to(targetPosition.global_position) <= safeDist:
-				global_position = targetPosition.global_position
+			# when target is reached, take next step or idle again if out of steps
+			if global_position.distance_squared_to(targetPosition) <= safeDist:
+				global_position = targetPosition
 				print("reached target")
 				
 				if movesLeft > 0:
 					Move()
 				else:
 					ChangeState(states.IDLE)
+					
 		states.IDLE:
 			pass
 	
+# function used to change an animal's state
 func ChangeState(newState):
 	prevState = state
 	state = newState
 	
 	EnterState(newState)
-	
+
+# function called every time the animal changes state
 func EnterState(enteredState):
 	match enteredState:
 		states.IDLE:
@@ -69,12 +97,18 @@ func EnterState(enteredState):
 		states.MOVING:
 			Move()
 	
+# calculates target position and steps remaining
 func Move():
 	var calcMovement = moveDistance/stepsPerMovement
-	targetPosition.global_position = global_position + Vector2(0, -calcMovement)
+	targetPosition = global_position + Vector2(0, -calcMovement)
 	movesLeft -= 1
 	
+	#print("current pos: ", global_position)
+	#print("target pos: ", targetPosition)
+	#print("moves left ", movesLeft)
+
+# called once the movement timer runs out
+# sets steps remaining and kicks off the movement loop
 func MoveTimeout():
-	print("dumbass")
 	movesLeft = stepsPerMovement
 	ChangeState(states.MOVING)
--- Scripts/Chicken.gd
+++ Scripts/Chicken.gd
@@ -2,8 +2,4 @@ extends Animal
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	bruh(100, 2, 1, 1, 100)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	Spawn(50, 2, 1, 1, 100, Vector2(500, 500))
